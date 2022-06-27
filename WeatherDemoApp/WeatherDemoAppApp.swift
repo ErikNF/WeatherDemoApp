1793a634a846a98303d5c8a08cae233dc031cedf
ErikNF <105495458+ErikNF@users.noreply.github.com>
2022-06-27 15:39:26 +0500

    Initial Commit

---
+++ WeatherDemoApp/WeatherDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoAppApp.swift
+//  WeatherDemoApp
+//
+//  Created by Эрик Нагаев on 27.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
